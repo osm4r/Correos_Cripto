f0bf1546937a3a000a8b348cd45a77785e652051
osm4r <103228889+osm4r@users.noreply.github.com>
2022-11-14 22:20:15 -0600

    upd cer

--- certificados/main.py
+++ certificados/main.py
@@ -2,6 +2,9 @@ import base64
 from nacl.encoding import HexEncoder
 from nacl.exceptions import BadSignatureError
 from nacl.signing import SigningKey
+from getpass import getpass
+import os
+
 
 def encrypt(data: bytes, password: bytes):
     encrypted_array: list = []
@@ -13,6 +16,7 @@ def encrypt(data: bytes, password: bytes):
             i=0
     return b''.join(encrypted_array)
 
+
 def decrypt(data: bytes, password: bytes):
     decrypted_array: list = []
     i=0
@@ -23,39 +27,55 @@ def decrypt(data: bytes, password: bytes):
             i=0
     return b''.join(decrypted_array)
 
+
 def write(data: bytes, path: str):
-    with open(path, "wb") as file:
+    with open(f"usuarios/{path}", "wb") as file:
         file.write(data)
 
+
 def read(path: bytes):
-    with open(path, "rb") as file:
+    with open(f"usuarios/{path}", "rb") as file:
         return file.read()
+
 
 def genKeyPair():
     kp = SigningKey.generate()
     return kp._seed
 
+
 def bytesToString(data: bytes):
     return base64.encodebytes(data).decode("utf-8")
 
+
 def stringToBytes(data: str):
     return base64.decodebytes(data.encode("utf-8"))
+
 
 def sign(msg: str, seed: bytes):
     sign_key = SigningKey(seed)
     signed_raw = sign_key.sign(msg.encode("utf-8"))
     return signed_raw
 
+
 def register(username: str, password: str):
     seed: bytes = genKeyPair()
     signed: dict = sign(username, seed)
-    write(encrypt(seed, password.encode("utf-8")), "dykeiichi.key")
+    try:
+        os.mkdir("usuarios")
+    except Exception as e:
+        print(e)
+    try:
+        os.mkdir(f"usuarios/{username}")
+    except Exception as e:
+        print(e)
+    write(encrypt(seed, password.encode("utf-8")), f"{username}/{username}.key")
     print(signed)
-    write(signed, "dykeiichi.cer")
+    write(signed, f"{username}/{username}.cer")
 
-def login(password: str):
-    seed: bytes = decrypt(read("dykeiichi.key"), password.encode("utf-8"))
-    signed_raw: bytes = read("dykeiichi.cer")
+
+def login(username: str, password: str):
+    seed: bytes = decrypt(read(f"{username}/{username}.key"), password.encode("utf-8"))
+    signed_raw: bytes = read(f"{username}/{username}.cer")
     print(signed_raw)
     verify_key = SigningKey(seed).verify_key
     print(verify_key._key)
@@ -65,9 +85,28 @@ def login(password: str):
     except BadSignatureError:
         return False
 
+
+def op_menu(op):
+    user = str(input("Usuario: "))
+    pswd = str(getpass("Contraseña: "))
+    if op == 1:
+        print(login(user, pswd))
+    else:
+        register(user, pswd)
+
+
 def main():
-    register("dykeiichi", "1234567890")
-    print(login("1234567890"))
+    print("---Elige una opción---")
+    print("1. Iniciar sesión")
+    print("2. Registrarse")
+    op = int(input("op: "))
+    if op == 1 or op == 2:
+        op_menu(op)
+    else:
+        print("opción inválida")
+    # register("dykeiichi", "1234567890")
+    # print(login("dykeiichi", "1234567890"))
+
 
 if __name__ == "__main__":
     main()
