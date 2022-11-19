88cd2c584d68b17cbf546cf1a0b92e405e658397
osm4r <103228889+osm4r@users.noreply.github.com>
2022-11-19 17:09:16 -0600

    .

--- python-SC-Ganache/mods/smart_contract_actions.py
+++ python-SC-Ganache/mods/smart_contract_actions.py
@@ -48,13 +48,13 @@ def deploy(address, private_key):
     # address = "0x10f1AEA2815e23D387E93b5fFe329877224a3905"
     # private_key = "37b61a20694ab7e489b00fcb516630986bbc92343a28cec72ecc4d61de85d7f6"
     # Create the contract in Python
-    ContactList = w3.eth.contract(abi=abi, bytecode=bytecode)
+    CorreoContract = w3.eth.contract(abi=abi, bytecode=bytecode)
     # Get the number of latest transaction
     nonce = w3.eth.getTransactionCount(address)
 
 
     # build transaction
-    transaction = ContactList.constructor().buildTransaction(
+    tx = CorreoContract.constructor().buildTransaction(
         {
             "chainId": chain_id,
             "gasPrice": w3.eth.gas_price,
@@ -63,37 +63,38 @@ def deploy(address, private_key):
         }
     )
     # Sign the transaction
-    sign_transaction = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+    sign_tx = w3.eth.account.sign_transaction(tx, private_key=private_key)
     print("Deploying Contract!")
     # Send the transaction
-    transaction_hash = w3.eth.send_raw_transaction(sign_transaction.rawTransaction)
+    tx_hash = w3.eth.send_raw_transaction(sign_tx.rawTransaction)
     # Wait for the transaction to be mined, and get the transaction receipt
     print("Waiting for transaction to finish...")
-    transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
-    print(f"Done! Contract deployed to {transaction_receipt.contractAddress}")
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
 
     
 def interact(address, private_key):
     abi = get_abi()
-    contact_list = w3.eth.contract('0xb46d64dA44674Feb8c522b557967362570745a10', abi=abi) # address=transaction_receipt.contractAddress, 
+    CorreoContract = w3.eth.contract('0xb46d64dA44674Feb8c522b557967362570745a10', abi=abi) # address=tx_receipt.contractAddress, 
     nonce = w3.eth.getTransactionCount(address)
-    store_contact = contact_list.functions.enviarCorreo(
+    enviarCorreo = CorreoContract.functions.enviarCorreo(
         "Asunto", "Body del correo xd", "0x37BdcD6908178ad42c281f20c9d458eAd567f112"
     ).buildTransaction({"chainId": chain_id, "from": address, "gasPrice": w3.eth.gas_price, "nonce": nonce})
     
 
     # Sign the transaction
-    sign_store_contact = w3.eth.account.sign_transaction(
-        store_contact, private_key=private_key
+    sign_function= w3.eth.account.sign_transaction(
+        enviarCorreo, private_key=private_key
     )
     # Send the transaction
-    send_store_contact = w3.eth.send_raw_transaction(sign_store_contact.rawTransaction)
-    transaction_receipt = w3.eth.wait_for_transaction_receipt(send_store_contact)
-    print(transaction_receipt)
+    send_function = w3.eth.send_raw_transaction(sign_function.rawTransaction)
+    tx_receipt = w3.eth.wait_for_transaction_receipt(send_function)
+    print(tx_receipt)
 
 
 def call(address, private_key):
     abi = get_abi()
-    contact_list = w3.eth.contract('0xb46d64dA44674Feb8c522b557967362570745a10', abi=abi)
-    print(contact_list.functions.leerCorreo().call())
+    CorreoContract = w3.eth.contract('0xb46d64dA44674Feb8c522b557967362570745a10', abi=abi)
+    leerCorreo = CorreoContract.functions.leerCorreo().call()
+    print(leerCorreo)
     # menu(True)
