010f42b0dee1022644840c157b219febb59348cf
may018 <103234851+may018@users.noreply.github.com>
2022-11-22 14:22:49 -0600

    Update mots_tkinter.py

--- proyecto/mods/mots_tkinter.py
+++ proyecto/mods/mots_tkinter.py
@@ -6,6 +6,170 @@ from tkinter import *
 from tkinter import messagebox,ttk
 from idlelib.tooltip import Hovertip
 
+
+def ventana_inicio():
+    global ventana_principal
+    pestas_color="DarkGrey"
+    ventana_principal=Tk()
+    ventana_principal.geometry("300x250")#DIMENSIONES DE LA VENTANA
+    ventana_principal.title("Login con tkinter")#TITULO DE LA VENTANA
+    Label(text="Escoja su opción", bg="LightGreen", width="300", height="2", font=("Calibri", 13)).pack()#ETIQUETA CON TEXTO
+    Label(text="").pack()
+    Button(text="Acceder", height="2", width="30", bg=pestas_color, command=login).pack() #BOTÓN "Acceder"
+    Label(text="").pack()
+    Button(text="Registrarse", height="2", width="30", bg=pestas_color, command=registro).pack() #BOTÓN "Registrarse".
+    Label(text="").pack()
+    ventana_principal.mainloop()
+
+def registro():
+    global ventana_registro
+    ventana_registro = Toplevel(ventana_principal)
+    ventana_registro.title("Registro")
+    ventana_registro.geometry("300x250")
+ 
+    global user
+    global privKey
+    global entrada_user
+    global entrada_privKey
+    user = StringVar() 
+    privKey = StringVar() 
+ 
+    Label(ventana_registro, text="Introduzca datos", bg="LightGreen").pack()
+    Label(ventana_registro, text="").pack()
+    etiqueta_user = Label(ventana_registro, text="Nombre de usuario * ")
+    etiqueta_user.pack()
+    entrada_nombre = Entry(ventana_registro, textvariable=user) #ESPACIO PARA INTRODUCIR EL NOMBRE.
+    entrada_nombre.pack()
+    etiqueta_privKey = Label(ventana_registro, text="Contraseña * ")
+    etiqueta_privKey.pack()
+    entrada_privKey = Entry(ventana_registro, textvariable=privKey, show='*') #ESPACIO PARA INTRODUCIR LA PRIVKEY.
+    entrada_privKey.pack()
+    Label(ventana_registro, text="").pack()
+    Button(ventana_registro, text="Registrarse", width=10, height=1, bg="LightGreen", command = registro_usuario).pack() #BOTÓN "Registrarse"
+
+#CREAMOS VENTANA PARA LOGIN.
+
+def login():
+    global ventana_login
+    ventana_login = Toplevel(ventana_principal)
+    ventana_login.title("Acceso a la cuenta")
+    ventana_login.geometry("300x250")
+    Label(ventana_login, text="Introduzca nombre de usuario y contraseña").pack()
+    Label(ventana_login, text="").pack()
+ 
+    global verifica_usuario
+    global verifica_privKey
+ 
+    verifica_usuario = StringVar()
+    verifica_privKey = StringVar()
+ 
+    global entrada_login_usuario
+    global entrada_login_privKey
+ 
+    Label(ventana_login, text="Nombre usuario * ").pack()
+    entrada_login_usuario = Entry(ventana_login, textvariable=verifica_usuario)
+    entrada_login_usuario.pack()
+    Label(ventana_login, text="").pack()
+    Label(ventana_login, text="Contraseña * ").pack()
+    entrada_login_privKey = Entry(ventana_login, textvariable=verifica_privKey, show= '*')
+    entrada_login_privKey.pack()
+    Label(ventana_login, text="").pack()
+    Button(ventana_login, text="Acceder", width=10, height=1, command = verifica_login).pack()
+
+#VENTANA "VERIFICACION DE LOGIN".
+
+def verifica_login():
+    usuario1 = verifica_usuario.get()
+    privKey1 = verifica_privKey.get()
+    entrada_login_usuario.delete(0, END) #BORRA INFORMACIÓN DEL CAMPO "Nombre usuario *" AL MOSTRAR NUEVA VENTANA.
+    entrada_login_privKey.delete(0, END) #BORRA INFORMACIÓN DEL CAMPO "Contraseña *" AL MOSTRAR NUEVA VENTANA.
+ 
+    lista_archivos = os.system('cls') #TAMBIEN TENGO DUDA AQUI CON LA INFO GENERADA SI AQUI ES EL ARCHIVO QUE REVISA
+    if usuario1 in lista_archivos:
+        archivo1 = open(usuario1, "r") #APERTURA DE ARCHIVO EN MODO LECTURA
+        verifica = archivo1.read().splitlines() #LECTURA DEL ARCHIVO QUE CONTIENE EL nombre Y contraseña.
+        #SI LA CONTRASEÑA INTRODUCIDA SE ENCUENTRA EN EL ARCHIVO...
+        if privKey1 in verifica:
+            exito_login() #...EJECUTAR FUNCIÓN "exito_login()"
+        #SI LA CONTRASEÑA NO SE ENCUENTRA EN EL ARCHIVO....
+        else:
+            no_privKey() #...EJECUTAR "no_privKey()"
+    else:
+        no_usuario() #..EJECUTA "no_usuario()".
+
+
+# VENTANA "Login finalizado con exito".
+ 
+def exito_login():
+    global ventana_exito
+    ventana_exito = Toplevel(ventana_login)
+    ventana_exito.title("Exito")
+    ventana_exito.geometry("150x100")
+    Label(ventana_exito, text="Login finalizado con exito").pack()
+    Button(ventana_exito, text="Entrar a contract menu", command=contract_menu).pack()
+ 
+#VENTANA DE "Contraseña incorrecta".
+ 
+def no_privKey():
+    global ventana_no_privKey
+    ventana_no_privKey = Toplevel(ventana_login)
+    ventana_no_privKey.title("ERROR")
+    ventana_no_privKey.geometry("150x100")
+    Label(ventana_no_privKey, text="Contraseña incorrecta ").pack()
+    Button(ventana_no_privKey, text="OK", command=borrar_no_privKey).pack() #EJECUTA "borrar_no_privKey()".
+ 
+#VENTANA DE "Usuario no encontrado".
+ 
+def no_usuario():
+    global ventana_no_usuario
+    ventana_no_usuario = Toplevel(ventana_login)
+    ventana_no_usuario.title("ERROR")
+    ventana_no_usuario.geometry("150x100")
+    Label(ventana_no_usuario, text="Usuario no encontrado").pack()
+    Button(ventana_no_usuario, text="OK", command=borrar_no_usuario).pack() #EJECUTA "borrar_no_usuario()"
+
+#CERRADO DE VENTANAS
+
+def borrar_exito_login():
+    ventana_exito.destroy()
+ 
+ 
+def borrar_no_privKey():
+    ventana_no_privKey.destroy()
+ 
+ 
+def borrar_no_usuario():
+    ventana_no_usuario.destroy()
+
+#REGISTRO USUARIO
+ 
+def registro_usuario(): ##AQUI TENGO DUDA CON LO QUE HACE EL REGISTRO, LO MANDO AL DOCUMENTO PERO NO SE 
+## SI LE DA LA PRIVKEY AUTOMATICO 
+ 
+    usuario_info = user.get()
+ 
+    address, privKey = create_account()
+        register(user, address, privKey)
+        print(f'Usuario {user} registrado correctamente')
+        print('Guarda tu private key porque se borrará la pantalla en 8 segundos')
+
+        with open(f'usernames.txt', 'a') as file:
+            file.write(f'{user}\n')
+ 
+    entrada_nombre.delete(0, END)
+    entrada_privKey.delete(0, END)
+ 
+    Label(ventana_registro, text="Registro completado con éxito", fg="green", font=("calibri", 11)).pack()
+ 
+
+ 
+ventana_inicio()  
+ 
+
+
+
+
+
 def show_selection(opc):
     # Obtener la opción seleccionada.
     ventana2 = tk.Tk()
