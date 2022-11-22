a19e3fd561556ac3d0c99eabf539e6503354f578
PHernandez04 <99228295+PHernandez04@users.noreply.github.com>
2022-11-22 01:46:09 -0600

    Update CorreoContract.sol

--- proyecto/contracts/CorreoContract/CorreoContract.sol
+++ proyecto/contracts/CorreoContract/CorreoContract.sol
@@ -1,20 +1,47 @@
 pragma solidity ^0.8.0;
 
 contract Correo_contract {
-    struct Correo_struct {
+
+    struct Correo_struct_Enviados {
+        address remitente;
+        address destinatario;
         string asunto;
         string body;
+        string fecha;
+    }
+    struct Correo_struct_Recibidos {
+        address remitente;
+        address destinatario;
+        string asunto;
+        string body;
+        string fecha;
     }
 
-    mapping(address => Correo_struct[]) correos;
+    mapping(address => Correo_struct_Enviados[]) correose;
+    mapping(address => Correo_struct_Recibidos[]) correosr;
 
-    function enviarCorreo(string memory asunto, string memory body, address destinatario) public {
-        Correo_struct[] storage correos_local = correos[destinatario];
-        correos_local.push(Correo_struct(asunto, body));
+    
+    function enviarCorreo(address remitente, string memory asunto, string memory body, address destinatario, string memory fecha) public {
+        Correo_struct_Recibidos[] storage correos_local = correosr[destinatario];
+        correos_local.push(Correo_struct_Recibidos(remitente, destinatario, asunto, body, fecha));
+        Correo_struct_Enviados[] storage correos_locall = correose[remitente];
+        correos_locall.push(Correo_struct_Enviados(remitente, destinatario, asunto, body, fecha));
     }
 
-    function leerCorreo(address micorreo) public view returns (Correo_struct[] memory) {
-        Correo_struct[] memory correos_local = correos[micorreo];
+    function leerCorreosRecibidos(address micorreo) public view returns (Correo_struct_Recibidos[] memory){
+        Correo_struct_Recibidos[] memory correos_local = correosr[micorreo];
         return correos_local;
     }
+
+
+    function leerBandejaEntrada(address micorreo) public view returns (Correo_struct_Enviados[] memory){
+        Correo_struct_Enviados[] memory correos_locall = correose[micorreo];
+        return correos_locall;
+    }
+
+    function eliminarBandejaEntrada(address micorreo) public {
+        require (keccak256(abi.encode(correose[micorreo])) != keccak256(abi.encode("")), "Error, this address is not registered yet");
+        delete correose[micorreo];
+    }
+
 }
