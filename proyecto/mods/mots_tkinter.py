b515c1140789e481af906872f3949bb89f84ae14
JJRivera9 <103225759+JJRivera9@users.noreply.github.com>
2022-11-23 03:54:58 -0600

    nueva configuracion

--- proyecto/mods/mots_tkinter.py
+++ proyecto/mods/mots_tkinter.py
@@ -7,9 +7,16 @@ from tkinter import messagebox,ttk
 from idlelib.tooltip import Hovertip
 from functools import partial
 
-def menu():
+def menu(ruser, raddress, rpassword):
     global ventana1
     global opc
+    global user
+    global address
+    global password
+    user = ruser
+    address = raddress
+    password = rpassword
+
     ventana1 = tk.Tk()
     ventana1.title("---ACCIONES DE SAMRT CONTRACT---")
     ventana1.config(width=500, height=250)
@@ -23,16 +30,25 @@ def menu():
 
         values=["1", "2", "3", "4", "5"]
 
-    )
 
+    )
     opc.place(x=120, y=150)
-    enviar = ttk.Button(ventana1, text="Enviar", command = partial( show_selection, opc))
+    enviar = ttk.Button(ventana1, text="Enviar", command = partial( show_selection, opc,) )
     enviar.place(x= 20, y=148)
+
 
     ventana1. mainloop()
 
 
-def menu2():
+def menu2(duser, daddress, dpassword):
+    global ventana
+    global opc
+    global user2
+    global address2
+    global password2
+    user2 = duser
+    address2 = daddress
+    password2 = dpassword
 
     ventana = tk.Tk()
     ventana.title("---ACCIONES DE SAMRT CONTRACT---")
@@ -45,17 +61,16 @@ def menu2():
         state="readonly",
         values=["1", "2"]
     )
-    opc.place(x=120, y=150)
+    opc.place(x=120, y=78)
     
-    enviar = ttk.Button(text="Enviar", 
-    command=show_selection2(opc))
-    enviar.place(x= 20, y=15)
+    enviar = ttk.Button(text="Enviar", command = partial( show_selection2, opc,))
+    enviar.place(x= 20, y=76)
 
     ventana. mainloop()
 
 def show_selection(opc):
     # Obtener la opción seleccionada.
-    
+    esconder(ventana1)
     if opc.get() == "1":
         ventana2 = Toplevel(ventana1)
         ventana2.config(width=800, height=500)
@@ -88,43 +103,144 @@ def show_selection(opc):
         enviar = ttk.Button(ventana2, text="Enviar")
         enviar.place(x=700,y= 260)
 
+
+        view = ttk.Button(ventana2, text = "Regresar", command = partial(mostrar))
+        view.place(rely=1.0, relx=1.0, x=-15, y=-15, anchor=SE)
+        ventana2.withdraw()
+
         ventana2.mainloop()
     elif opc.get() == "2":
         ventana3 = tk.Tk()
-        ventana3.config(width=500, height=250)
-  
-        txt1 = ttk.Label(ventana3, text = " Ingrese el codigo aqui(BORRAR ESTE CODIGO)")
-        txt1.place(x=20, y=20)
-     
+        ventana3.config(width=700, height=1000)
+
+        #pleerCorreosRecibidos = call_leerCorreosRecibidos(user, address)
+        pleerCorreosRecibidos = {0: {'Asunto': 'asdka',
+                                    'Body': 'laksjd alskjd asdlkj',
+                                    'Destinatario': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275',
+                                    'Fecha': '23/11/2022, 00:24:13',
+                                    'Remitente': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275'},
+    1: {'Asunto': 'osadjk asodj',
+        'Body': 'alksd alksjhd aslkdh',
+        'Destinatario': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275',
+        'Fecha': '23/11/2022, 00:27:38',
+        'Remitente': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275'},
+    2: {'Asunto': 'osmar',
+        'Body': 'jas;dj l;kasjdlkas alskhdasl',
+        'Destinatario': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275',
+        'Fecha': '23/11/2022, 00:27:49',
+        'Remitente': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275'}}
+        n = 20
+
+        for x in pleerCorreosRecibidos:
+            txt1 = ttk.Label(ventana3, text = f'Correo {x}')
+            txt1.place(x=10, y=n)
+            for key, y in pleerCorreosRecibidos[x].items():
+                txt1 = ttk.Label(ventana3, text = f'{key}: {y}')
+                txt1.place(x=74, y=n)
+                n = n+20
+            n += 30
+        
+        view = ttk.Button(ventana3, text = "Regresar", command = partial(mostrar, ventana1, ventana3) )
+        view.place(rely=1.0, relx=1.0, x=-15, y=-15, anchor=SE)
+
         ventana3.mainloop()
     elif opc.get() == "3":
         ventana4 = tk.Tk()
-        ventana4.config(width=500, height=250)
+        ventana4.config(width=700, height=1000)
   
-        txt1 = ttk.Label(ventana4, text = " Ingrese el codigo aqui(BORRAR ESTE CODIGO)")
-        txt1.place(x=20, y=20)
+        pleerCorreosEnviados = {0: {'Asunto': 'asdka',
+                                    'Body': 'laksjd alskjd asdlkj',
+                                    'Destinatario': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275',
+                                    'Fecha': '23/11/2022, 00:24:13',
+                                    'Remitente': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275'},
+    1: {'Asunto': 'osadjk asodj',
+        'Body': 'alksd alksjhd aslkdh',
+        'Destinatario': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275',
+        'Fecha': '23/11/2022, 00:27:38',
+        'Remitente': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275'},
+    2: {'Asunto': 'osmar',
+        'Body': 'jas;dj l;kasjdlkas alskhdasl',
+        'Destinatario': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275',
+        'Fecha': '23/11/2022, 00:27:49',
+        'Remitente': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275'}}
+        n = 20
+
+        for x in pleerCorreosEnviados:
+            txt1 = ttk.Label(ventana4, text = f'Correo {x}')
+            txt1.place(x=10, y=n)
+            for key, y in pleerCorreosEnviados[x].items():
+                txt1 = ttk.Label(ventana4, text = f'{key}: {y}')
+                txt1.place(x=74, y=n)
+                n = n+20
+            n += 30
      
+        view = ttk.Button(ventana4, text = "Regresar", command = partial(mostrar, ventana1, ventana4) )
+        view.place(rely=1.0, relx=1.0, x=-15, y=-15, anchor=SE)
+
         ventana4.mainloop()
     elif opc.get() == "4":
         ventana5 = tk.Tk()
-        ventana5.config(width=500, height=250)
+        ventana5.config(width=700, height=1000)
   
-        txt1 = ttk.Label(ventana5, text = " Ingrese el codigo aqui(BORRAR ESTE CODIGO)")
-        txt1.place(x=20, y=20)
+        EliminarBandejaEntrada = {0: {'Asunto': 'asdka',
+                                    'Body': 'laksjd alskjd asdlkj',
+                                    'Destinatario': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275',
+                                    'Fecha': '23/11/2022, 00:24:13',
+                                    'Remitente': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275'},
+        1: {'Asunto': 'osadjk asodj',
+            'Body': 'alksd alksjhd aslkdh',
+            'Destinatario': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275',
+            'Fecha': '23/11/2022, 00:27:38',
+            'Remitente': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275'},
+        2: {'Asunto': 'osmar',
+            'Body': 'jas;dj l;kasjdlkas alskhdasl',
+            'Destinatario': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275',
+            'Fecha': '23/11/2022, 00:27:49',
+            'Remitente': '0x31d163DB5eF525fcDEE93d3CF9133531C353F275'}}
+        n = 20
+
+        for x in EliminarBandejaEntrada:
+            txt1 = ttk.Label(ventana5, text = f'Correo {x}')
+            txt1.place(x=10, y=n)
+            for key, y in EliminarBandejaEntrada[x].items():
+                txt1 = ttk.Label(ventana5, text = f'{key}: {y}')
+                txt1.place(x=74, y=n)
+                n = n+20
+            n += 30
+        view = ttk.Button(ventana5, text = "Regresar", command = partial(mostrar, ventana1, ventana5) )
+        view.place(rely=1.0, relx=1.0, x=-15, y=-15, anchor=SE)
+
      
         ventana5.mainloop()
     else:
-        quit()   
+        quit()  
+    mostrar() 
 
 def show_selection2(opc):
+    esconder(ventana)
     # Obtener la opción seleccionada.
-    if opc.get() == "2":
-        selection = opc.get()
-        messagebox.showinfo(
-        message=f"La opción seleccionada es: {selection}",
-        title="Selección")
+    if opc.get() == "1":
+
+        ventana6 = tk.Tk()
+        ventana6.config(width=500, height=250)
+        pdeploy = "Hola mundo!"
+        #pdeploy = deploy(address, privKey)  
+        txt1 = ttk.Label(ventana6, text = pdeploy)
+        txt1.pack(expand=True, fill=tk.BOTH, padx=10, pady=10)
+
+        view = ttk.Button(ventana6, text = "Regresar", command = partial(mostrar, ventana, ventana6) )
+        view.place(rely=1.0, relx=1.0, x=-15, y=-15, anchor=SE)
+     
+        ventana6.mainloop()
     else:
         quit()
 
-menu()
+def esconder(view):
+    view.withdraw()
+def mostrar(view, view2):
+    view.deiconify()
+    esconder(view2)
+
+menu("quser", "qaddress", "qpassword")
+
 
